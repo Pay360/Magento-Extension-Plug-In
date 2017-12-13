81c17ab3c94d8d463aab43c98de93893e85ee3aa
Thanh, Nguyen Duy <nd.thanh@outlook.com>
2017-12-13 15:24:39 +0700

    finish schema installation script

--- Pay360/Payments/Setup/InstallSchema.php
+++ Pay360/Payments/Setup/InstallSchema.php
@@ -39,75 +39,279 @@ class InstallSchema implements InstallSchemaInterface
         $installer->startSetup();
 
         $table_pay360_payments_profile = $setup->getConnection()->newTable($setup->getTable('pay360_payments_profile'));
-
-        
         $table_pay360_payments_profile->addColumn(
-            'profile_id',
+            'id',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
             array('identity' => true,'nullable' => false,'primary' => true,'unsigned' => true,),
-            'Entity ID'
-        );
-        
-
-        
-        $table_pay360_payments_profile->addColumn(
-            'id',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            'Profile ID'
+        )
+        ->addColumn(
+            'customer_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
+            array('nullable' => false, 'unsigned' => true,),
+            'Customer ID'
+        )
+        ->addColumn(
+            'profile_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            15,
             [],
-            'id'
+            'Profile Id'
+        )
+        ->addColumn(
+            'card_token',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            15,
+            [],
+            'Card Token'
+        )
+        ->addColumn(
+            'registered',
+            \Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
+            1,
+            [],
+            'Registered Customer'
+        )
+        ->addColumn(
+            'card_type',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            20,
+            [],
+            'Card Type'
+        )
+        ->addColumn(
+            'card_usage_type',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            20,
+            [],
+            'Card Usage Type'
+        )
+        ->addColumn(
+            'card_scheme',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            20,
+            [],
+            'Card Scheme'
+        )
+        ->addColumn(
+            'card_category',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            20,
+            [],
+            'Card Category'
+        )
+        ->addColumn(
+            'masked_pan',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            20,
+            [],
+            'Masked PAN'
+        )
+        ->addColumn(
+            'expiry_date',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            6,
+            [],
+            'Expiry Date'
+        )
+        ->addColumn(
+            'issuer',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            30,
+            [],
+            'Issuer'
+        )
+        ->addColumn(
+            'issuer_country',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            30,
+            [],
+            'Issuer Country'
+        )
+        ->addColumn(
+            'card_holder_name',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            250,
+            [],
+            'Card Holder Name'
+        )
+        ->addColumn(
+            'card_nick_name',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            150,
+            [],
+            'Card Nick Name'
+        )
+        ->addIndex(
+            $installer->getIdxName('pay360_profile_id', ['id']),
+            ['id']
+        )
+        ->addIndex(
+            $installer->getIdxName('pay360_card_token', ['card_token']),
+            ['card_token']
+        )
+        ->addForeignKey(
+            $installer->getFkName('pay360_payments_profile', 'customer_id', 'customer_entity', 'entity_id'),
+            'customer_id',
+            $installer->getTable('customer_entity'),
+            'entity_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
         );
-        
 
         $table_pay360_payments_session = $setup->getConnection()->newTable($setup->getTable('pay360_payments_session'));
-
-        
         $table_pay360_payments_session->addColumn(
-            'session_id',
+            'id',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
             array('identity' => true,'nullable' => false,'primary' => true,'unsigned' => true,),
             'Entity ID'
-        );
-        
-
-        
-        $table_pay360_payments_session->addColumn(
-            'id',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        )
+        ->addColumn(
+            'order_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
+            array('nullable' => false, 'unsigned' => true,),
+            'Order ID'
+        )
+        ->addColumn(
+            'session_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            30,
             [],
-            'id'
+            'Session ID'
+        )
+        ->addColumn(
+            'session_date',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Session Date'
+        )
+        ->addColumn(
+            'last_modified',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Last Modified'
+        )
+        ->addColumn(
+            'status',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            15,
+            [],
+            'Status'
+        )
+        ->addIndex(
+            $installer->getIdxName('pay360_session_session_id', ['session_id']),
+            ['session_id']
+        )
+        ->addForeignKey(
+            $installer->getFkName('pay360_payments_session', 'order_id', 'sales_order', 'entity_id'),
+            'order_id',
+            $installer->getTable('sales_order'),
+            'entity_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
         );
-        
+
 
         $table_pay360_payments_transaction = $setup->getConnection()->newTable($setup->getTable('pay360_payments_transaction'));
-
-        
         $table_pay360_payments_transaction->addColumn(
-            'transaction_id',
+            'id',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
             array('identity' => true,'nullable' => false,'primary' => true,'unsigned' => true,),
-            'Entity ID'
-        );
-        
-
-        
-        $table_pay360_payments_transaction->addColumn(
-            'id',
+            'ID'
+        )
+        ->addColumn(
+            'transaction_id',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            null,
+            30,
             [],
-            'id'
+            'Transaction Id'
+        )
+        ->addColumn(
+            'deferred',
+            \Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
+            1,
+            [],
+            'Deferred'
+        )
+        ->addColumn(
+            'merchant_ref',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            15,
+            [],
+            'Merchant Reference'
+        )
+        ->addColumn(
+            'merchant_description',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            [],
+            'Merchant Description'
+        )
+        ->addColumn(
+            'type',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            20,
+            [],
+            'Transaction Type'
+        )
+        ->addColumn(
+            'amount',
+            \Magento\Framework\DB\Ddl\Table::TYPE_DECIMAL,
+            '12,4',
+            [],
+            'Transaction Amount'
+        )
+        ->addColumn(
+            'status',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            15,
+            [],
+            'Transaction Status'
+        )
+        ->addColumn(
+            'currency',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            10,
+            [],
+            'Transaction Currency'
+        )
+        ->addColumn(
+            'transaction_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Transaction Time'
+        )
+        ->addColumn(
+            'received_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Received Time'
+        )
+        ->addColumn(
+            'channel',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            20,
+            [],
+            'Transaction Channel'
+        )
+        ->addIndex(
+            $installer->getIdxName('pay360_profile_id', ['id']),
+            ['id']
+        )
+        ->addIndex(
+            $installer->getIdxName('pay360_profile_transaction_id', ['transaction_id']),
+            ['transaction_id']
         );
-        
 
         $setup->getConnection()->createTable($table_pay360_payments_transaction);
-
         $setup->getConnection()->createTable($table_pay360_payments_session);
-
         $setup->getConnection()->createTable($table_pay360_payments_profile);
 
         $setup->endSetup();
