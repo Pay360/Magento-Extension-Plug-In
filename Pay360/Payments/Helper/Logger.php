3d4f8c6c741920c8350741f6a42bc4f1da85745f
Thanh, Nguyen Duy <nd.thanh@outlook.com>
2018-01-01 18:10:07 +0700

    update logger

--- Pay360/Payments/Helper/Logger.php
+++ Pay360/Payments/Helper/Logger.php
@@ -10,18 +10,20 @@ namespace  Pay360\Payments\Helper;
 class Logger
 {
     const MAGE_CODE = '/app/code/';
+    const LOG_FILE = '/var/log/pay360debug.log';
 
     protected $_enabled = false; 
     protected $_config;
-    protected $_systemlogger;
+    protected $_psrLogger;
 
     function __construct(
-        \Magento\Payment\Model\Method\Logger $systemlogger,
+        \Psr\Log\LoggerInterface $psrLogger,
         \Pay360\Payments\Model\Config $config
     )
     {
         $this->_config = $config;
-        $this->_systemlogger = $systemlogger;
+        $this->_psrLogger = $psrLogger;
+        $this->_psrLogger->pushHandler(new \Monolog\Handler\StreamHandler(self::LOG_FILE));
         $this->_enabled = $this->_config->getValue('payment/pay360/test');
     }
 
@@ -38,22 +40,22 @@ class Logger
                 $file = $last_step['file'];
                 list($pwd, $file) = explode(self::MAGE_CODE, $file);
                 $line = $last_step['line'];
-                $this->_systemlogger->debug("Called From ".self::MAGE_CODE."{$file}:{$line}");
+                $this->_psrLogger->debug("Called From ".self::MAGE_CODE."{$file}:{$line}");
             }
             catch (\Exception $e) {
-                $this->_systemlogger->debug(array('error' => $e->getMessage()));
+                $this->_psrLogger->debug(array('error' => $e->getMessage()));
             }
 
             if (is_array($content)) {
                 foreach ($content as $key=>$value) {
                     if ($key && $value) {
-                        $this->_systemlogger->debug(array($key => $value));
+                        $this->_psrLogger->debug(array($key => $value));
                     }
                 }
             }
             else {
                 $content = strval($content);
-                $this->_systemlogger->debug(array('content' => $content));
+                $this->_psrLogger->debug(array('content' => $content));
             }
         }
     }
